c568ccc8300cf8d9216b42857fa67153015bb74b
Marcel Humes <mhumes@concentrichx.com>
2022-06-06 20:16:12 -0400

    Commiting once again

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+
+function App() {
+    return ( <div className = "App" >
+
+    
+              <div className="container">
+               <div className="logo">Logo</div>
+               <div className="headers">Headers</div>
+               <div className="nav">Nav</div>
+               <div className="content">Content</div>
+               <div className="footer">Footer</div>
+              </div>
+
+            </div>
+    );
+}
+
+export default App;
